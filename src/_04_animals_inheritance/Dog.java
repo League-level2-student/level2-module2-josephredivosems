16635053843649cc3d8e887315d1b7fbed6a0f42
student <student@ip-172-31-52-156.us-west-2.compute.internal>
2021-05-05 16:25:41 -0700

    hi

--- src/_04_animals_inheritance/Dog.java
+++ src/_04_animals_inheritance/Dog.java
@@ -1,5 +1,22 @@
 package _04_animals_inheritance;
 
 public class Dog {
+	String name;
+	String furColor;
+	boolean isGirl;
+	Dog(String name, String furColor, boolean isGirl){
+		this.name=name;
+		this.furColor=furColor;
+		this.isGirl=isGirl;
+	}
+	public void printName() {
+		System.out.println(name);
+	}
+	public void play() {
+		System.out.println("playing");
+	}
+	public void eat() {
+		
+	}
 	
 }
